0d1095e55f5b1e700ddaab500cd18c7b0f09e863
Nur Islam <nurislamceo@gmail.com>
2024-01-11 00:56:48 +0600

    added missung directory call
    templates/   added missung directory call

--- create-page.php
+++ create-page.php
@@ -1,5 +1,5 @@
 <div class="wrap">
     <h1 class="wp-heading-inline">Add Team Member</h1>
 
-    <?php include plugin_dir_path(__FILE__) . 'add-team-member-form.php'; ?>
+    <?php include plugin_dir_path(__FILE__) . 'templates/add-team-member-form.php'; ?>
 </div>
